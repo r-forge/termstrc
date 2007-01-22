0d59fa187536d34d80b7192d62007823b561acb1
rbrt (Robert Ferstl) <datarob@gmx.at>
2007-01-22 14:38:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@4 b6e8c802-fa29-48c1-b741-14f4e927981d

---
+++ presentations/EURO/auto/termstrcusr2006.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook "termstrcusr2006"
+ (lambda ()
+    (LaTeX-add-bibitems
+     "BIS2005"
+     "Bolder1999"
+     "Geyer1999"
+     "Jankowitsch2004"
+     "Nelson1987"
+     "Svensson1994")
+    (LaTeX-add-labels
+     "accruedinterest"
+     "bondpriceeq"
+     "yield"
+     "bondprceq2"
+     "forwrate"
+     "duration"
+     "spread"
+     "nelson")
+    (TeX-run-style-hooks
+     "bm"
+     "amsbsy"
+     "amsthm"
+     "amsmath"
+     "amssymb"
+     "fontenc"
+     "T1"
+     "times"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "handout"
+     "mathserif"
+     "10pt")))
+
