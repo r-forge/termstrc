eed7bd1a691709af13cddb663fccf9f041c45295
joe (Josef Hayden) <josef.hayden@gmail.com>
2007-05-21 08:57:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@49 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc/R/splines.R
+++ termstrc/R/splines.R
@@ -11,8 +11,6 @@ splines_estim <-
     
   # select given group from bonddata
   bonddata <- bonddata[group]
-
-  
   
   # select data according to chosen maturity range
   if (length(maturity_spectrum)==1) {bonddata <- bonddata }else
@@ -25,120 +23,101 @@ splines_estim <-
   cf <- lapply(bonddata,create_cashflows_matrix)
 
   # create cashflows matrix including dirty price (needed for bond yield calculation)
-  cf_p <- mapply(function(i) create_cashflows_matrix(bonddata[[i]],include_price=TRUE),
+  cf_p <- mapply(function(k) create_cashflows_matrix(bonddata[[k]],include_price=TRUE),
                  1:n_group,SIMPLIFY=FALSE)
-
-  names(cf_p) <- names(bonddata)
-
+    
   # create maturities matrix
   m <- lapply(bonddata,create_maturities_matrix)
 
   # create maturities matrix including zeros (needed for bond yield calculation)
-  m_p <- mapply(function(i) create_maturities_matrix(bonddata[[i]],include_price=TRUE),
+  m_p <- mapply(function(k) create_maturities_matrix(bonddata[[k]],include_price=TRUE),
                 1:n_group,SIMPLIFY=FALSE)
-  names(m_p) <- names(bonddata)
-  
+    
   # calculate dirty prices
-  p <- mapply(function(i) bonddata[[i]]$PRICE + bonddata[[i]]$ACCRUED,1:n_group,SIMPLIFY=FALSE)
-  names(p) <- names(bonddata)   
-
-  positions <- mapply(function(i) order(apply(m[[i]],2,max)),1:n_group,SIMPLIFY=FALSE)
-  names(positions) <- names(bonddata)
-
-
-  cf <- mapply(function(i) cf[[i]][,positions[[i]]],1:n_group,SIMPLIFY=FALSE)
-  cf_p <- mapply(function(i) cf_p[[i]][,positions[[i]]],1:n_group,SIMPLIFY=FALSE)
-  m <- mapply(function(i) m[[i]][,positions[[i]]],1:n_group,SIMPLIFY=FALSE)
-  m_p <- mapply(function(i) m_p[[i]][,positions[[i]]],1:n_group,SIMPLIFY=FALSE)
- 
+  p <- mapply(function(k) bonddata[[k]]$PRICE + bonddata[[k]]$ACCRUED,1:n_group,SIMPLIFY=FALSE)
   
+  # index for ordering
+  positions <- mapply(function(k) order(apply(m[[k]],2,max)),1:n_group,SIMPLIFY=FALSE)
+  
+  # order matrices 
+  cf <- mapply(function(k) cf[[k]][,positions[[k]]],1:n_group,SIMPLIFY=FALSE)
+  cf_p <- mapply(function(k) cf_p[[k]][,positions[[k]]],1:n_group,SIMPLIFY=FALSE)
+  m <- mapply(function(k) m[[k]][,positions[[k]]],1:n_group,SIMPLIFY=FALSE)
+  m_p <- mapply(function(k) m_p[[k]][,positions[[k]]],1:n_group,SIMPLIFY=FALSE)
+    
   # calculate bond yields	
-  yields <- mapply(function(i) bond_yields(cf_p[[i]],m_p[[i]]),
+  yields <- mapply(function(k) bond_yields(cf_p[[k]],m_p[[k]]),
                    1:n_group,SIMPLIFY=FALSE)
-  names(yields) <- names(bonddata)
-
+    
+  # Choosing knot points (McCulloch)
+  K <- mapply(function(k) ncol(m[[k]]),1:n_group,SIMPLIFY=FALSE)
+    
+  # number of basis functions
+  s <-  mapply(function(k) round(sqrt(K[[k]])),1:n_group,SIMPLIFY=FALSE)
+    
+  # only used for knot point finding
+  i <- mapply(function(k) 2:(s[[k]]-2),1:n_group,SIMPLIFY=FALSE)  
   
-# Choosing knot points (McCulloch)
-
-K <- mapply(function(i) ncol(m[[i]]),1:n_group,SIMPLIFY=FALSE)
-names(K) <- names(bonddata)
+  h <-  mapply(function(k) trunc(((i[[k]]-1)*K[[k]])/(s[[k]]-2)),1:n_group,SIMPLIFY=FALSE)
+             
+  theta <- mapply(function(k)((i[[k]]-1)*K[[k]])/(s[[k]]-2)-h[[k]],1:n_group,SIMPLIFY=FALSE)
+    
+  # knot points
+  T <- mapply(function(k) c(0,
+       apply(as.matrix(m[[k]][,h[[k]]]),2,max)
+       +theta[[k]]*(apply(as.matrix(m[[k]][,h[[k]]+1]),2,max)-apply(as.matrix(m[[k]][,h[[k]]]),2,max)),
+       max(m[[k]][,ncol(m[[k]])])),1:n_group,SIMPLIFY=FALSE)
+ 
+  # use own knot points
+  # T <- seq(0,15,1.5)
+  # s <- length(T)+1
   
-# number of basis functions
-s <-  mapply(function(i) round(sqrt(K[[i]])),1:n_group,SIMPLIFY=FALSE)
-names(s) <- names(bonddata)
-  
-i <- mapply(function(i) 2:(s[[i]]-2),1:n_group,SIMPLIFY=FALSE)  # only used for knot point finding
-names(i) <- names(bonddata)
+  # parameter estimation with OLS
+  y <- mapply(function(k) apply(cf_p[[k]],2,sum),1:n_group,SIMPLIFY=FALSE)
+   
+  X <- list()
 
-h <-  mapply(function(j) trunc(((i[[j]]-1)*K[[j]])/(s[[j]]-2)),1:n_group,SIMPLIFY=FALSE)
-names(h) <- names(bonddata)
-            
-theta <- mapply(function(j)((i[[j]]-1)*K[[j]])/(s[[j]]-2)-h[[j]],1:n_group,SIMPLIFY=FALSE)
-names(theta) <- names(bonddata)
-
-  
-# knot points
-T <- mapply(function(i) c(0,
-       apply(as.matrix(m[[i]][,h[[i]]]),2,max)
-       +theta[[i]]*(apply(as.matrix(m[[i]][,h[[i]]+1]),2,max)-apply(as.matrix(m[[i]][,h[[i]]]),2,max)),
-       max(m[[i]][,ncol(m[[i]])])),1:n_group,SIMPLIFY=FALSE)
-names(T) <- names(bonddata)
-
-# use own knot points
-# T <- seq(0,15,1.5)
-# s <- length(T)+1
-  
-# parameter estimation with OLS
-y <- mapply(function(i) apply(cf_p[[i]],2,sum),1:n_group,SIMPLIFY=FALSE)
-names(y) <- names(bonddata)
-
-  
-X <- list()
-
-# k ... group index
-# j ... column index (bond)
-# sidx ... index for spline function  
-
-
-for (k in 1:n_group){  
-X[[k]] <- matrix(NA,ncol(m[[k]]),s[[k]])
+  # k ... group index
+  # j ... column index (bond)
+  # sidx ... index for spline function  
+ 
+  for (k in 1:n_group){  
+  X[[k]] <- matrix(NA,ncol(m[[k]]),s[[k]])
                  
-for(sidx in 1:s[[k]]){
-X[[k]][,sidx] <- apply(cf[[k]]*mapply(function(j) gi(m[[k]][,j],T[[k]],sidx,s[[k]]),1:ncol(m[[k]])),2,sum)
-}
-}
-
-names(X) <- names(bonddata)
-  
-alpha <- mapply(function(k) coef(lm(-y[[k]]~X[[k]]-1)),1:n_group) # parameter vector
-
-dt <- list()
-  
-for (k in 1:n_group){
-  dt[[k]] <- matrix(1,nrow(m[[k]]),ncol(m[[k]]))
-  for(sidx in 1:s[[k]]){
-  dt[[k]] <- dt[[k]] + alpha[[k]][sidx]* mapply(function(j) gi(m[[k]][,j],T[[k]],sidx,s[[k]]),1:ncol(m[[k]]))
+   for(sidx in 1:s[[k]]){
+   X[[k]][,sidx] <- apply(cf[[k]]*mapply(function(j) gi(m[[k]][,j],T[[k]],sidx,s[[k]]),1:ncol(m[[k]])),2,sum)
+   }
   }
-}  
+  
+  alpha <- mapply(function(k) coef(lm(-y[[k]]~X[[k]]-1)),1:n_group) # parameter vector
 
-browser()
+  dt <- list()
+  
+  for (k in 1:n_group){
+   dt[[k]] <- matrix(1,nrow(m[[k]]),ncol(m[[k]]))
+   for(sidx in 1:s[[k]]){
+    dt[[k]] <- dt[[k]] + alpha[[k]][sidx]* mapply(function(j) gi(m[[k]][,j],T[[k]],sidx,s[[k]]),1:ncol(m[[k]]))
+   }
+  }  
 
-k <-1
-t <- seq(0,30,0.5)
+  browser()
 
-dt2 <- list()
-dt2[[k]] <- rep(1,length(t))
+ k <-1
+ t <- seq(0,30,0.5)
+
+ dt2 <- list()
+ dt2[[k]] <- rep(1,length(t))
                
-for(sidx in 1:s[[k]]){  
-dt2[[k]] <- dt2[[k]] + alpha[[k]][sidx]*gi(t,T[[k]],sidx,s[[k]])
-}
+ for(sidx in 1:s[[k]]){  
+ dt2[[k]] <- dt2[[k]] + alpha[[k]][sidx]*gi(t,T[[k]],sidx,s[[k]])
+ }
 
-spot_rates <- -log(dt2[[k]])/t           # estimated yields
+ spot_rates <- -log(dt2[[k]])/t           # estimated yields
 
-estimated_prices <- list()
-estimated_prices <- mapply(function(k) apply(cf[[k]]*dt[[k]],2,sum),1:n_group,SIMPLIFY=FALSE)
+ estimated_prices <- list()
+ estimated_prices <- mapply(function(k) apply(cf[[k]]*dt[[k]],2,sum),1:n_group,SIMPLIFY=FALSE)
 
-yhat <- mapply(function(k) bond_yields(rbind(-estimated_prices[[k]],cf[[k]]),m_p[[k]]),1:n_group,SIMPLIFY=FALSE)
+ yhat <- mapply(function(k) bond_yields(rbind(-estimated_prices[[k]],cf[[k]]),m_p[[k]]),1:n_group,SIMPLIFY=FALSE)
 
 
  k = 2 
