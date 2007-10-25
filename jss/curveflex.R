23c2f4efa5a944c992322aa4d8f7543d5ab955c7
joe <joe@b6e8c802-fa29-48c1-b741-14f4e927981d>
2007-10-25 17:44:08 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@132 b6e8c802-fa29-48c1-b741-14f4e927981d

--- jss/curveflex.R
+++ jss/curveflex.R
@@ -27,12 +27,40 @@ legend("topright",legend=c(expression(beta[1]*exp(-m/tau[1])),
        lty=c(4,2,3,1), col=c(1,2,4,3))
 
 
-spotrate <- function(m,b){ b[1]+ b[2]*((1-exp(-m/b[4]))/(m/b[4]))+ b[3]*(((1-exp(-m/b[4]))/(m/b[4]))-exp(-m/b[4]))}
-
-b <- c( 5,1,1,1)
+nelson <- function(m,b){ b[1]+ b[2]*((1-exp(-m/b[4]))/(m/b[4]))+ b[3]*(((1-exp(-m/b[4]))/(m/b[4]))-exp(-m/b[4]))}
 
 b <- cbind(5,-1,-12:12,1)
 
 
-plot(m,spotrate(m,b[1,]), ylim=c(-1,8),type="l")
-for(i in 2:nrow(b)) lines(m,spotrate(m,b[i,]))
+plot(m, nelson(m,b[1,]), ylim=c(-1,8),type="l")
+for(i in 2:nrow(b)) lines(m,nelson(m,b[i,]))
+
+svensson <-
+  function(beta, m) {
+  (beta[1] + beta[2] * ((1 - exp(-m/beta[4]))/(m/beta[4])) +
+  beta[3] * (((1 - exp(-m/beta[4]))/(m/beta[4])) - exp(-m/beta[4])) +
+  beta[5] * (((1 - exp(-m/beta[6]))/(m/beta[6])) - exp(-m/beta[6])))}
+
+b <- cbind(5,-1,1,1,4,4:0,1)
+
+
+for(i in 2:nrow(b)) lines(m,svensson(b[i,],m))
+
+par(mfrow=c(2,4))
+plot(m, svensson(b[1,],m),type="l")
+
+plot(m,svensson(c(5,-1,1,1,1,1,10),m), type="l")
+
+plot(m,svensson(c(5,-4,-15,1,0.1,6,4),m), type="l")
+
+#plot(m,svensson(c(5,-4,-1,10,4,0.5,7),m), type="l")
+plot(m,svensson(c(5,-4,-1,15,4,0.5,4),m), type="l")
+
+plot(m,svensson(c(5,-4,-1,10,15,0.5,1),m), type="l")
+
+plot(m,svensson(c(5,-4,-5,15,15,0.1,1),m), type="l")
+
+plot(m,svensson(c(5,-4,-5,15,10,2,10),m), type="l")
+
+plot(m,svensson(c(5,-4,1 ,1,4,2,1),m), type="l")
+
