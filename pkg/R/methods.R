099769d4035c4353b54f3c3be9ed0228ec790b0c
joe (Josef Hayden) <josef.hayden@gmail.com>
2008-02-14 16:38:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@238 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/R/methods.R
+++ pkg/R/methods.R
@@ -316,7 +316,7 @@ plot.cubicsplines <-
     	  # knot points 
     	  abline(v=c(x$T[[k]]),lty=2, col="darkgrey")
     	  legend("bottom",legend=c("Zero-coupon yield curve",
-    	  "95 % Confidence intervall" ,"Yield to maturity", "Knot points"),
+    	  "95 % Confidence interval" ,"Yield to maturity", "Knot points"),
     	  col=c("steelblue","steelblue","red", "darkgrey"),
     	  lty = c(1,3,-1,2), pch=c(-1,-1,21,-1))
 	
