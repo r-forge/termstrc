e54037989e539b1b4c27c20269d65f47ef1aede1
rbrt (Robert Ferstl) <datarob@gmx.at>
2010-01-17 19:08:35 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@375 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc2/code/demo_couponbonds.R
+++ termstrc2/code/demo_couponbonds.R
@@ -1,10 +1,15 @@
- load("govbonds.RData")
+load("govbonds.RData")
 ns_res <- estim_nss(govbonds, c("GERMANY", "AUSTRIA", "FRANCE"),matrange = c(0,30), method = "ns", deltatau = 0.2)
 print(ns_res)
 plot(ns_res)
 summary(ns_res)
 
+## Svensson estimation with parameter grid search (can take some time, increase deltatau for wider grid)
 sv_res <- estim_nss(govbonds, c("GERMANY"),matrange = c(0,30), method = "sv", deltatau = 1)
 print(sv_res)
 plot(sv_res)
 summary(sv_res)
+
+## Plot startparameter grid search results
+plot(sv_res$spsearch$GERMANY)
+
