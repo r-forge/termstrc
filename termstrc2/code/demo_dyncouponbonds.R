5ec72e3f8e0d0e68c6b440b8fce48d1aaf917649
joe (Josef Hayden) <josef.hayden@gmail.com>
2010-01-18 15:27:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@392 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc2/code/demo_dyncouponbonds.R
+++ termstrc2/code/demo_dyncouponbonds.R
@@ -1,24 +1,5 @@
 ## Import CSVs
 datadyncouponbonds <- dyncouponbonds(c("DataGermany S.csv", "DataGermany AC.csv", "DataGermany CP.csv"), "GERMANY")
-datadyncouponbonds <- datadyncouponbonds[10:55]
-class(datadyncouponbonds) <- "dyncouponbonds"
-
-## Diebold/Li estimation
-dl_res <- estim_nss(datadyncouponbonds, c("GERMANY"), method = "dl")
-print(dl_res)
-summary(dl_res)
-
-## Nelson/Siegel estimation
-ns_res <- estim_nss(datadyncouponbonds, c("GERMANY"), method = "ns", deltatau = 0.5)
-print(ns_res)
-plot(ns_res)
-
-tsparam <- param(ns_res)
-plot(tsparam)
-
-
-## Plot startparameter grid search results
-plot(ns_res[[1]]$spsearch$GERMANY)
 
 ## Svensson estimation
 sv_res <- estim_nss(datadyncouponbonds, c("GERMANY"), method = "sv", deltatau = 2)
@@ -29,7 +10,12 @@ summary(sv_res)
 ## Plot startparameter grid search results
 plot(sv_res[[1]]$spsearch$GERMANY)
 
+## Plot parameters
 
+plot(param(sv_res))
+summary(param(sv_res))
+
+     
 
 
 
