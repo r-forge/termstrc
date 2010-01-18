3e90a594cbe8284f062cd8108ea4f43c11369d60
rbrt <rbrt@b6e8c802-fa29-48c1-b741-14f4e927981d>
2010-01-18 14:28:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@389 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc2/code/demo_dyncouponbonds.R
+++ termstrc2/code/demo_dyncouponbonds.R
@@ -1,19 +1,29 @@
-rm(list = ls())
-
-source("termstrcPackage.R")
-
 ## Import CSVs
 datadyncouponbonds <- dyncouponbonds(c("DataGermany S.csv", "DataGermany AC.csv", "DataGermany CP.csv"), "GERMANY")
+
+## Diebold/Li estimation
+dl_res <- estim_nss(datadyncouponbonds, c("GERMANY"), method = "dl")
+print(dl_res)
+summary(dl_res)
 
 ## Nelson/Siegel estimation
 ns_res <- estim_nss(datadyncouponbonds, c("GERMANY"), method = "ns", deltatau = 0.5)
 print(ns_res)
+plot(ns_res)
 summary(ns_res)
+
+## Plot startparameter grid search results
+plot(ns_res[[1]]$spsearch$GERMANY)
 
 ## Svensson estimation
 sv_res <- estim_nss(datadyncouponbonds, c("GERMANY"), method = "sv", deltatau = 2)
 print(sv_res)
+plot(sv_res)
 summary(sv_res)
+
+## Plot startparameter grid search results
+plot(sv_res[[1]]$spsearch$GERMANY)
+
 
 
 
