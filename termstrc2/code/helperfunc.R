def9268f254117a937e574f27e00514377ccebb5
rbrt <rbrt@b6e8c802-fa29-48c1-b741-14f4e927981d>
2010-01-21 17:33:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@434 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc2/code/helperfunc.R
+++ termstrc2/code/helperfunc.R
@@ -1,17 +1,4 @@
 
-## Calculation of the duration,modified duration and duration based weights 
-duration <- function (cf_p,m_p,y){
-  y <- matrix(rep(y,nrow(m_p)),ncol=ncol(m_p),byrow=TRUE)
-  # mac cauly duration
-  d <- apply(cf_p*m_p*exp(-y*m_p),2,sum)/-cf_p[1,]
-  # modified duration
-  md <- d/(1+y[1,])
-  omega <- (1/d)/sum(1/d)
-  dur <- cbind(d,md,omega)
-  colnames(dur) <- c("Duration","Modified duration","Weights")
-  dur
-}
-
 ## Root mean squared error
 rmse <- function (actual,estimated) {
   e <- actual - estimated
@@ -25,7 +12,4 @@ aabse <-function (actual,estimated){
 }   							
 
 
-## Loss function: mean squared (weighted error)
-loss_function <- function(p,phat,omega) {
-  sum(omega*((p-phat)^2))
-}
+
