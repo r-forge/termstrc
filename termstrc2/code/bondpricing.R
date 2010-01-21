d94391e2da8d55fc76984032dd0917da82c7db3a
rbrt (Robert Ferstl) <datarob@gmx.at>
2010-01-21 17:33:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@434 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc2/code/bondpricing.R
+++ termstrc2/code/bondpricing.R
@@ -46,3 +46,21 @@ bond_yields <- function(cashflows, m, searchint=c(-1,1), tol=1e-10) {
   colnames(bondyields) <- c("Maturity","Yield")
   bondyields
 }
+
+## Calculation of the duration,modified duration and duration based weights 
+duration <- function (cf_p,m_p,y){
+  y <- matrix(rep(y,nrow(m_p)),ncol=ncol(m_p),byrow=TRUE)
+  # mac cauly duration
+  d <- apply(cf_p*m_p*exp(-y*m_p),2,sum)/-cf_p[1,]
+  # modified duration
+  md <- d/(1+y[1,])
+  omega <- (1/d)/sum(1/d)
+  dur <- cbind(d,md,omega)
+  colnames(dur) <- c("Duration","Modified duration","Weights")
+  dur
+}
+
+## Loss function: mean squared (weighted error)
+loss_function <- function(p,phat,omega) {
+  sum(omega*((p-phat)^2))
+}
