842333937e131cc67bacc42e560d77f65dbaaf6b
rbrt <rbrt@b6e8c802-fa29-48c1-b741-14f4e927981d>
2010-01-22 14:12:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@446 b6e8c802-fa29-48c1-b741-14f4e927981d

---
+++ termstrc2/code/rcmdcheck.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import os
+import shutil
+
+## Estimation kernel
+os.system("cp estim_cs.R ~/WORK/termstrc/pkg/R")
+os.system("cp estim_nss_couponbonds.R ~/WORK/termstrc/pkg/R")
+os.system("cp estim_nss_dyncouponbonds.R ~/WORK/termstrc/pkg/R")
+os.system("cp estim_nss_zeroyields.R ~/WORK/termstrc/pkg/R")
+os.system("cp bondpricing.R ~/WORK/termstrc/pkg/R")
+
+## Parametric forms of spot rate functions
+os.system("cp spotfwdratedef.R ~/WORK/termstrc/pkg/R")
+os.system("cp cubicfunc.R ~/WORK/termstrc/pkg/R")
+
+## Data handling
+os.system("cp couponbonds_data.R ~/WORK/termstrc/pkg/R")
+os.system("cp create_cf_m.R ~/WORK/termstrc/pkg/R")
+
+## Methods for estimation results
+os.system("cp methods_curves.R ~/WORK/termstrc/pkg/R")
+os.system("cp methods_dyntermstrc_nss.R ~/WORK/termstrc/pkg/R")
+os.system("cp methods_dyntermstrc_param.R ~/WORK/termstrc/pkg/R")
+os.system("cp methods_dyntermstrc_yields.R ~/WORK/termstrc/pkg/R")
+os.system("cp methods_termstrc_cs.R ~/WORK/termstrc/pkg/R")
+os.system("cp methods_termstrc_nss.R ~/WORK/termstrc/pkg/R")
+os.system("cp methods_zeroyields.R ~/WORK/termstrc/pkg/R")
+os.system("cp gof.R ~/WORK/termstrc/pkg/R")
+
+os.system("cp -R  ~/WORK/termstrc/termstrc2/helpfiles/*.Rd  ~/WORK/termstrc/pkg/man")
+
+## Perform R CMD check
+os.system("R CMD check ~/WORK/termstrc/pkg")
