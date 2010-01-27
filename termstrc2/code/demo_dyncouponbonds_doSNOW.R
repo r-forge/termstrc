d00d8ae2e24df4a41a5c06f57cc5be4dbfdf6a3b
rbrt (Robert Ferstl) <datarob@gmx.at>
2010-01-27 17:02:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@475 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc2/code/demo_dyncouponbonds_doSNOW.R
+++ termstrc2/code/demo_dyncouponbonds_doSNOW.R
@@ -3,17 +3,43 @@ load("GermanBonds.RData")
 
 NODES <- 5 
 
-library("snow")
+require("snow")
 cl <- makeCluster(NODES, type = "MPI")
 
-library(doSNOW)
+require(doSNOW)
 registerDoSNOW(cl)
 
 N <- length(datadyncouponbonds)
-N <- 10
+N <- 5
 
-sv_res <- foreach(i = 1:N) %dopar% { 
-  sv_res <- estim_nss(datadyncouponbonds[[i]], c("GERMANY"), method = "sv", deltatau = 1)
+sv_res <- foreach(i = 1:N, packages = c("rgl", "urca", "sandwich")) %dopar% { 
+  ## Estimation kernel
+source("estim_cs.R")
+source("estim_nss_couponbonds.R")
+source("estim_nss_dyncouponbonds.R")
+source("estim_nss_zeroyields.R")
+source("bondpricing.R")
+
+## Parametric forms of spot rate functions
+source("spotfwdratedef.R")
+source("cubicfunc.R")
+
+## Data handling
+source("couponbonds_data.R")
+source("create_cf_m.R")
+source("methods_couponbonds.R")
+
+## Methods for estimation results
+source("methods_curves.R")
+source("methods_dyntermstrc_nss.R")
+source("methods_dyntermstrc_param.R")
+source("methods_dyntermstrc_yields.R")
+source("methods_termstrc_cs.R")
+source("methods_termstrc_nss.R")
+source("methods_zeroyields.R")
+source("gof.R")
+source("factorcontrib.R")  
+sv_res <- estim_nss(datadyncouponbonds[[i]], c("GERMANY"), method = "sv", deltatau = 1)
 }
 
 stopCluster(cl)
