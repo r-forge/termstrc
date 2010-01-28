dac965426af6325dd64482f12bcb1d550eece6fb
rbrt (Robert Ferstl) <datarob@gmx.at>
2010-01-28 11:25:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@490 b6e8c802-fa29-48c1-b741-14f4e927981d

---
+++ termstrc2/code/demo_dyncouponbonds_doSNOW3.R
@@ -0,0 +1,23 @@
+library("dyntermstrc")
+
+data("GermanBonds")
+
+NODES <- 20 
+
+require("snow")
+cl <- makeCluster(NODES, type = "MPI")
+
+require(doSNOW)
+registerDoSNOW(cl)
+
+N <- length(datadyncouponbonds)
+
+ns_res <- foreach(i = 1:N, .packages = "dyntermstrc") %dopar% {  
+estim_nss(datadyncouponbonds[[i]], c("GERMANY"), method = "ns", deltatau = 0.2)
+}
+
+stopCluster(cl)
+
+class(asv_res) <- "dyntermstrc_nss"
+
+save(ns_res, file = "demo_dyncouponbonds_doSNOW3.RData")
