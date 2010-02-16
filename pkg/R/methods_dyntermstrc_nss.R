0de7bb0b829a79f8bfd16f21a37ce70b0b2d3229
joe (Josef Hayden) <josef.hayden@gmail.com>
2010-02-16 13:29:29 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@511 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/R/methods_dyntermstrc_nss.R
+++ pkg/R/methods_dyntermstrc_nss.R
@@ -37,7 +37,7 @@ print.summary.dyntermstrc_nss <- function(x,...) {
     cat("Goodness of fit:\n")
     cat("---------------------------------------------------\n")
 
-     print.default(format(x$gof,digi=6),quote=FALSE)
+     print.default(format(x$gof,digits=6),quote=FALSE)
 
     cat("\n")
     cat("---------------------------------------------------\n")
