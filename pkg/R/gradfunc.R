44c6e20befa75b666c06c2b3f64fff75826aa9da
joe (Josef Hayden) <josef.hayden@gmail.com>
2010-02-17 14:23:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@522 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/R/gradfunc.R
+++ pkg/R/gradfunc.R
@@ -14,20 +14,20 @@ grad_sv_bonds_grid <- function(beta, tau, m, cf, w, p){
   a <- exp((-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m)*m/100)
 
   a[is.nan(a)] <- 0
-  b <- -2*w*(p-apply(a*cf,2,sum))
+  b <- -2*w*(p-colSums(a*cf))
   d <- a*cf/100
   dm <- d*m
   
-  gbeta1 <- sum(b*(-apply(dm,2,sum)))
-  gbeta2 <- sum(b*(-apply(d*tau[1]*(1-exp(-m/tau[1])),2,sum)))
+  gbeta1 <- sum(b*(-colSums(dm)))
+  gbeta2 <- sum(b*(-colSums(d*tau[1]*(1-exp(-m/tau[1])))))
 
   b3 <- dm*(-exp(-m/tau[1]) +tau[1]*(1-exp(-m/tau[1]))/m)
   b3[is.nan(b3)] <- 0
-  gbeta3 <- sum(b*(-apply(b3,2,sum)))
+  gbeta3 <- sum(b*(-colSums(b3)))
   
   b5 <- dm*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m)
   b5[is.nan(b5)] <- 0
-  gbeta5 <- sum(b*(-apply(b5,2,sum)))            
+  gbeta5 <- sum(b*(-colSums(b5)))            
 
 
   c(gbeta1,gbeta2,gbeta3,gbeta5)
