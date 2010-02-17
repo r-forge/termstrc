5473eb80a3c2faca7b46538d788efdeb96f6c217
joe <joe@b6e8c802-fa29-48c1-b741-14f4e927981d>
2010-02-17 15:50:26 +0000

    2
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@527 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/R/gradfunc.R
+++ pkg/R/gradfunc.R
@@ -13,8 +13,12 @@ grad_sv_bonds_grid <- function(beta, tau, m, cf, w, p){
 
   emt1 <- exp(-m/tau[1])
   emt2 <- exp(-m/tau[2])
+  t1emt1 <- tau[1]*(1 - emt1)
+  emt1tm <- (-emt1 + t1emt1/m)
+  emt2tm <- (-emt2 + tau[2]*(1 - emt2)/m)
+  
 
-  a <- exp((-beta[1] - beta[3]*(-emt1 + (tau[1]*(1 - emt1))/m) - beta[4]*(-emt2 + (tau[2]*(1 - emt2))/m) - (beta[2]*tau[1]*(1 - emt1))/m)*m/100)
+  a <- exp((-beta[1] - beta[3]*emt1tm - beta[4]*emt2tm - (beta[2]*t1emt1)/m)*m/100)
 
  
   acf <- a*cf
@@ -23,9 +27,9 @@ grad_sv_bonds_grid <- function(beta, tau, m, cf, w, p){
   dm <- d*m
   
   gbeta1 <- sum(b*(-colSums(dm,na.rm=TRUE)))
-  gbeta2 <- sum(b*(-colSums(d*tau[1]*(1-emt1),na.rm=TRUE)))
-  gbeta3 <- sum(b*(-colSums(dm*(-emt1 +tau[1]*(1-emt1)/m),na.rm=TRUE)))
-  gbeta5 <- sum(b*(-colSums(dm*(-emt2 + (tau[2]*(1 - emt2))/m),na.rm=TRUE)))            
+  gbeta2 <- sum(b*(-colSums(d*t1emt1,na.rm=TRUE)))
+  gbeta3 <- sum(b*(-colSums(dm*emt1tm, na.rm=TRUE)))
+  gbeta5 <- sum(b*(-colSums(dm*emt2tm, na.rm=TRUE)))            
 
 
   c(gbeta1,gbeta2,gbeta3,gbeta5)
@@ -64,3 +68,12 @@ grad_sv_bonds <- function(beta,tau,m,cf,w,p){
   
 }
 
+
+
+cSums <- function (x, na.rm = FALSE, dims = 1L) {
+    dn <- dim(x)
+    n <- prod(dn[1L:dims])
+    dn <- dn[-(1L:dims)]
+    z <-  .Internal(colSums(x, n, prod(dn), na.rm))
+    z
+}
