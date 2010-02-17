1818d7da09f1e2563d6aae5a67db4767c8ee4d78
joe (Josef Hayden) <josef.hayden@gmail.com>
2010-02-17 14:32:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@523 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/R/gradfunc.R
+++ pkg/R/gradfunc.R
@@ -11,7 +11,10 @@
 
 grad_sv_bonds_grid <- function(beta, tau, m, cf, w, p){
 
-  a <- exp((-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m)*m/100)
+  emt1 <- exp(-m/tau[1])
+  emt2 <- exp(-m/tau[2])
+
+  a <- exp((-beta[1] - beta[3]*(-emt1 + (tau[1]*(1 - emt1))/m) - beta[4]*(-emt2 + (tau[2]*(1 - emt2))/m) - (beta[2]*tau[1]*(1 - emt1))/m)*m/100)
 
   a[is.nan(a)] <- 0
   b <- -2*w*(p-colSums(a*cf))
@@ -19,13 +22,13 @@ grad_sv_bonds_grid <- function(beta, tau, m, cf, w, p){
   dm <- d*m
   
   gbeta1 <- sum(b*(-colSums(dm)))
-  gbeta2 <- sum(b*(-colSums(d*tau[1]*(1-exp(-m/tau[1])))))
+  gbeta2 <- sum(b*(-colSums(d*tau[1]*(1-emt1))))
 
-  b3 <- dm*(-exp(-m/tau[1]) +tau[1]*(1-exp(-m/tau[1]))/m)
+  b3 <- dm*(-emt1 +tau[1]*(1-emt1)/m)
   b3[is.nan(b3)] <- 0
   gbeta3 <- sum(b*(-colSums(b3)))
   
-  b5 <- dm*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m)
+  b5 <- dm*(-emt2 + (tau[2]*(1 - emt2))/m)
   b5[is.nan(b5)] <- 0
   gbeta5 <- sum(b*(-colSums(b5)))            
 
@@ -39,28 +42,28 @@ grad_sv_bonds <- function(beta,tau,m,cf,w,p){
    a <- exp((-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m)*m/100)
 
   a[is.nan(a)] <- 0
-  b <- -2*w*(p-apply(a*cf,2,sum))
+  b <- -2*w*(p-colSums(a*cf))
   d <- a*cf/100
   dm <- d*m
   
-  gbeta1 <- sum(b*(-apply(dm,2,sum)))
-  gbeta2 <- sum(b*(-apply(d*tau[1]*(1-exp(-m/tau[1])),2,sum)))
+  gbeta1 <- sum(b*(-colSums(dm)))
+  gbeta2 <- sum(b*(-colSums(d*tau[1]*(1-exp(-m/tau[1])))))
 
   b3 <- dm*(-exp(-m/tau[1]) +tau[1]*(1-exp(-m/tau[1]))/m)
   b3[is.nan(b3)] <- 0
-  gbeta3 <- sum(b*(-apply(b3,2,sum)))
+  gbeta3 <- sum(b*(-colSums(b3)))
 
   b4 <- dm*(-beta[2]*exp(-m/tau[1])/tau[1] + beta[2]*(1-exp(-m/tau[1]))/m + beta[3]*(-exp(-m/tau[1])/tau[1]+ (1-exp(-m/tau[1]))/m - exp(-m/tau[1])*m/tau[1]^2))
   b4[is.nan(b4)] <- 0
-  gbeta4 <- sum(b*(-apply(b4,2,sum)))
+  gbeta4 <- sum(b*(-colSums(b4)))
    
   b5 <- dm*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m)
   b5[is.nan(b5)] <- 0
-  gbeta5 <- sum(b*(-apply(b5,2,sum)))   
+  gbeta5 <- sum(b*(-colSums(b5)))   
 
   b6 <- dm*beta[4]*( -exp(-m/tau[2])/tau[2] + (1-exp(-m/tau[2]))/m - exp(-m/tau[2])*m/tau[2]^2)
   b6[is.nan(b6)] <- 0 
-  gbeta6 <- sum(b*(-apply(b6,2,sum)))
+  gbeta6 <- sum(b*(-colSums(b6)))
 
   c(gbeta1,gbeta2,gbeta3,gbeta4,gbeta5,gbeta6)
   
