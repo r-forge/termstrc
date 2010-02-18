16c2c11cff7788c0aaeed8c669a10b662161d2da
rbrt <rbrt@b6e8c802-fa29-48c1-b741-14f4e927981d>
2010-02-18 18:59:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@534 b6e8c802-fa29-48c1-b741-14f4e927981d

--- devel/test_rcpp.R
+++ devel/test_rcpp.R
@@ -52,6 +52,7 @@ objfct_sv_bonds_grid <- function(beta, tau, m, cf, w, p) {
       loss_function(p, phat, w)
     }
 
+rm(grad_sv_bonds_gridC)
 grad_sv_bonds_gridCpp <- '
 	RcppVector<double> betac(beta);
 	RcppVector<double> tauc(tau);
@@ -59,43 +60,53 @@ grad_sv_bonds_gridCpp <- '
 	RcppMatrix<double> cfc(cf);
 	RcppVector<double> wc(w);
 	RcppVector<double> pc(p);
-        RcppVector<double> phat(pc.size()); // delete later
-
-        RcppMatrix<double> emt1(mc.rows(), mc.cols());
-        RcppMatrix<double> emt2(mc.rows(), mc.cols());
-        RcppMatrix<double> t1emt1(mc.rows(), mc.cols());
-        RcppMatrix<double> emt1tm(mc.rows(), mc.cols());
-        RcppMatrix<double> emt2tm(mc.rows(), mc.cols());
-
-        double gbeta1 = 0;
-        double gbeta2 = 0;
-	double gbeta3 = 0;
-        double gbeta5 = 0;
-
+	
+	const int N = mc.rows();
+	const int M = mc.cols();
+	
+	RcppMatrix<double> emt1(N, M);
+	RcppMatrix<double> emt2(N, M);
+	RcppMatrix<double> t1emt1(N, M);
+	RcppMatrix<double> emt1tm(N, M);
+	RcppMatrix<double> emt2tm(N, M);
+	RcppMatrix<double> acf(N, M);
+	RcppMatrix<double> dm(N, M);
+	RcppVector<double> csacf(M);
+	RcppVector<double> csdm(M);
+	RcppVector<double> csdt1emt1(M);
+	RcppVector<double> csdmem1tm(M);
+	RcppVector<double> csdmem2tm(M);
+	RcppVector<double> b(M);
+	Rcpp::NumericVector gbeta(4);
+	
 	int i = 0;
 	int j = 0;
-	double s;
-        double mse = 0;
-	
-	for (j = 0; j<mc.cols(); j++) {
-         // Rprintf("j is %d:\\n", j);
-                i = 0;
-		while (i<mc.rows() && mc(i,j)>0) {
-                //Rprintf("i is %d and mc(i,j) is %f:\\n", i, mc(i,j));
-			s =  (betac(0) + betac(1) * ((1 - exp(-mc(i,j)/tauc(0)))/(mc(i,j)/tauc(0))) +
-				  betac(2) * (((1 - exp(-mc(i,j)/tauc(0)))/(mc(i,j)/tauc(0))) - exp(-mc(i,j)/tauc(0))) +
-				  betac(3) * (((1 - exp(-mc(i,j)/tauc(1)))/(mc(i,j)/tauc(1))) - exp(-mc(i,j)/tauc(1))))/100;
-
-			phat(j) += cfc(i,j)*exp(-s*mc(i,j));
+		
+	for (j = 0; j<M; j++) {
+		i = 0;
+		while (i<N && mc(i,j)>0) {
+			emt1(i,j) = exp(-mc(i,j)/tauc(0));
+			emt2(i,j) = exp(-mc(i,j)/tauc(1));
+			t1emt1(i,j) = tauc(0)*(1 - emt1(i,j));
+			emt1tm(i,j) = - emt1(i,j) + t1emt1(i,j)/mc(i,j);
+			emt2tm(i,j) = - emt2(i,j) + tauc(1)*(1 - emt2(i,j))/mc(i,j);					   
+			acf(i,j) = cfc(i,j)*exp((-betac(0) - betac(2)*emt1tm(i,j) - betac(3)*emt2tm(i,j) - (betac(1)*t1emt1(i,j))/mc(i,j))*mc(i,j)/100);
+			dm(i,j) = acf(i,j)/100*mc(i,j);
+			csacf(j) += acf(i,j);
+			csdm(j) += dm(i,j) ;
+			csdt1emt1(j) += acf(i,j)/100*t1emt1(i,j);
+			csdmem1tm(j) += dm(i,j)*emt1tm(i,j);
+			csdmem2tm(j) += dm(i,j)*emt2tm(i,j);
 			i++;
 		}
-	mse += pow(pc(j) - phat(j),2)*wc(j);
+		b(j) = -2*wc(j)*(pc(j) - csacf(j));
+		gbeta[0] += b(j)*(-csdm(j));
+		gbeta[1] += b(j)*(-csdt1emt1(j));
+		gbeta[2] += b(j)*(-csdmem1tm(j));
+		gbeta[3] += b(j)*(-csdmem2tm(j));
 	}
-        Rcpp::NumericVector xa(4);
-        xa[0] = 4;
-        xa[3] = 99;
-	return xa;
-	//return Rcpp::wrap(mse);
+	
+	return gbeta;
         '
 grad_sv_bonds_gridC <- cfunction(signature(beta="numeric", tau="numeric", m="numeric", cf="numeric", w="numeric", p="numeric"), grad_sv_bonds_gridCpp, Rcpp=TRUE, verbose=FALSE)
 
@@ -114,17 +125,20 @@ grad_sv_bonds_grid <- function(beta, tau, m, cf, w, p){
 
  
   acf <- a*cf
-  b <- -2*w*(p-cSums(acf,na.rm=TRUE))
+  b <- -2*w*(p-cSums(acf,na.rm=TRUE)) # vector
   d <- acf/100
   dm <- d*m
   
   gbeta1 <- sum(b*(-cSums(dm,na.rm=TRUE)))
-  gbeta2 <- sum(b*(-cSums(d*t1emt1,na.rm=TRUE)))
+  gbeta2 <- sum(b*(-cSums(d*t1emt1, na.rm=TRUE)))
   gbeta3 <- sum(b*(-cSums(dm*emt1tm, na.rm=TRUE)))
-  gbeta5 <- sum(b*(-cSums(dm*emt2tm, na.rm=TRUE)))
-  
+  gbeta5 <- sum(b*(-cSums(dm*emt2tm, na.rm=TRUE)))            
+
+
   c(gbeta1,gbeta2,gbeta3,gbeta5)
 }
+
+grad_sv_bonds_grid(beta=beta, tau=tau, m=m, cf=cf, w=w, p=p)
 
 cSums <- function (x, na.rm = FALSE, dims = 1L) {
     dn <- dim(x)
@@ -139,3 +153,9 @@ objfct_sv_bonds_gridC(beta=beta, tau=tau, m=m, cf=cf, w=w, p=p)
 
 system.time(for (i in 1:10000) objfct_sv_bonds_gridC(beta=beta, tau=tau, m=m, cf=cf, w=w, p=p))
 system.time(for (i in 1:10000) objfct_sv_bonds_grid(beta=beta, tau=tau, m=m, cf=cf, w=w, p=p))
+
+grad_sv_bonds_gridC(beta=beta, tau=tau, m=m, cf=cf, w=w, p=p)
+grad_sv_bonds_grid(beta=beta, tau=tau, m=m, cf=cf, w=w, p=p)
+
+system.time(for (i in 1:10000) grad_sv_bonds_gridC(beta=beta, tau=tau, m=m, cf=cf, w=w, p=p))
+system.time(for (i in 1:10000) grad_sv_bonds_grid(beta=beta, tau=tau, m=m, cf=cf, w=w, p=p))
