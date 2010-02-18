740a04e3a9abb30ecc6bd2c4872405fa03bbb5cd
rbrt (Robert Ferstl) <datarob@gmx.at>
2010-02-18 18:59:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@534 b6e8c802-fa29-48c1-b741-14f4e927981d

--- devel/test_objfctgrid.R
+++ devel/test_objfctgrid.R
@@ -84,9 +84,68 @@ objfct_sv_bonds_gridCpp <- '
 
 objfct_sv_bonds_gridC <- cfunction(signature(beta="numeric", tau="numeric", m="numeric", cf="numeric", w="numeric", p="numeric"), objfct_sv_bonds_gridCpp, Rcpp=TRUE, verbose=FALSE)
 
+grad_sv_bonds_gridCpp <- '
+	RcppVector<double> betac(beta);
+	RcppVector<double> tauc(tau);
+	RcppMatrix<double> mc(m);
+	RcppMatrix<double> cfc(cf);
+	RcppVector<double> wc(w);
+	RcppVector<double> pc(p);
+	
+	const int N = mc.rows();
+	const int M = mc.cols();
+	
+	RcppMatrix<double> emt1(N, M);
+	RcppMatrix<double> emt2(N, M);
+	RcppMatrix<double> t1emt1(N, M);
+	RcppMatrix<double> emt1tm(N, M);
+	RcppMatrix<double> emt2tm(N, M);
+	RcppMatrix<double> acf(N, M);
+	RcppMatrix<double> dm(N, M);
+	RcppVector<double> csacf(M);
+	RcppVector<double> csdm(M);
+	RcppVector<double> csdt1emt1(M);
+	RcppVector<double> csdmem1tm(M);
+	RcppVector<double> csdmem2tm(M);
+	RcppVector<double> b(M);
+	Rcpp::NumericVector gbeta(4);
+	
+	int i = 0;
+	int j = 0;
+		
+	for (j = 0; j<M; j++) {
+		i = 0;
+		while (i<N && mc(i,j)>0) {
+			emt1(i,j) = exp(-mc(i,j)/tauc(0));
+			emt2(i,j) = exp(-mc(i,j)/tauc(1));
+			t1emt1(i,j) = tauc(0)*(1 - emt1(i,j));
+			emt1tm(i,j) = - emt1(i,j) + t1emt1(i,j)/mc(i,j);
+			emt2tm(i,j) = - emt2(i,j) + tauc(1)*(1 - emt2(i,j))/mc(i,j);					   
+			acf(i,j) = cfc(i,j)*exp((-betac(0) - betac(2)*emt1tm(i,j) - betac(3)*emt2tm(i,j) - (betac(1)*t1emt1(i,j))/mc(i,j))*mc(i,j)/100);
+			dm(i,j) = acf(i,j)/100*mc(i,j);
+			csacf(j) += acf(i,j);
+			csdm(j) += dm(i,j) ;
+			csdt1emt1(j) += acf(i,j)/100*t1emt1(i,j);
+			csdmem1tm(j) += dm(i,j)*emt1tm(i,j);
+			csdmem2tm(j) += dm(i,j)*emt2tm(i,j);
+			i++;
+		}
+		b(j) = -2*wc(j)*(pc(j) - csacf(j));
+		gbeta[0] += b(j)*(-csdm(j));
+		gbeta[1] += b(j)*(-csdt1emt1(j));
+		gbeta[2] += b(j)*(-csdmem1tm(j));
+		gbeta[3] += b(j)*(-csdmem2tm(j));
+	}
+	
+	return gbeta;
+        '
+grad_sv_bonds_gridC <- cfunction(signature(beta="numeric", tau="numeric", m="numeric", cf="numeric", w="numeric", p="numeric"), grad_sv_bonds_gridCpp, Rcpp=TRUE, verbose=FALSE)
+
+
 objfct_sv_bonds_grid(beta, tau, m, cf, w, p)
 objfct_sv_bonds_gridC(beta, tau, m, cf, w, p)
 grad_sv_bonds_grid(beta, tau, m, cf, w, p)
+grad_sv_bonds_gridC(beta, tau, m, cf, w, p)
 
 control = list()
 outer.iterations = 30
@@ -124,12 +183,12 @@ lsparam2 <- constrOptim(theta = rep(0.01,4),
 
 lsparam3 <- constrOptim(theta = rep(0.01,4),
                        f = objfct_sv_bonds_gridC,
-                       grad = NULL,
+                       grad = grad_sv_bonds_gridC,
                        ui = ui,
                        ci = ci,
                        mu = 1e-04,
                        control = control,
-                       method = "Nelder-Mead",
+                       method = "BFGS",
                        outer.iterations = outer.iterations,
                        outer.eps = outer.eps,
                        tau, m, cf, w, p) ## additional inputs for f and grad
@@ -190,6 +249,22 @@ test4 <- function() {
   for (i in 1:N) {
     lsparam2 <- constrOptim(theta = rep(0.01,4),
                         f = objfct_sv_bonds_gridC,
+                        grad = grad_sv_bonds_gridC,
+                        ui = ui,
+                        ci = ci,
+                        mu = 1e-04,
+                        control = control,
+                        method = "BFGS",
+                        outer.iterations = outer.iterations,
+                        outer.eps = outer.eps,
+                        tau, m, cf, w, p) ## additional inputs for f and grad
+  }
+}
+
+test5 <- function() {
+  for (i in 1:N) {
+    lsparam2 <- constrOptim(theta = rep(0.01,4),
+                        f = objfct_sv_bonds_gridC,
                         grad = grad_sv_bonds_grid,
                         ui = ui,
                         ci = ci,
@@ -201,10 +276,12 @@ test4 <- function() {
                         tau, m, cf, w, p) ## additional inputs for f and grad
   }
 }
+
 system.time(test1())
 system.time(test2())
 system.time(test3())
 system.time(test4())
+system.time(test5())
 
 
 objfct_sv_bonds_grid2 <- function(beta, tau, m, cf, w, p) {
@@ -239,6 +316,11 @@ test3 <- function() {
 Rprof()
 test3()
 Rprof(NULL)
+summaryRprof()
+
+Rprof()
+ for(i in 1:10000) grad_sv_bonds_gridC(beta,tau,m,cf,w,p)
+ Rprof(NULL)
 summaryRprof()
 
 Rprof()
