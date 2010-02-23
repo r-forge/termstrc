5c56942bf2de8a40b3ea3a0646c8f64b87315f72
rbrt (Robert Ferstl) <datarob@gmx.at>
2010-02-23 18:28:24 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@536 b6e8c802-fa29-48c1-b741-14f4e927981d

--- devel/test_estim_nss_couponbonds.R
+++ devel/test_estim_nss_couponbonds.R
@@ -8,12 +8,13 @@ source("gradfunc.R")
 data(govbonds)
 
 Rprof()
-sv_res2 <- estim_nss(govbonds, c("GERMANY", "AUSTRIA"), matrange = c(0,30), method = "sv")
+sv_res2 <- estim_nss(govbonds, c("GERMANY"), matrange = c(0,30), method = "sv")
   Rprof(NULL)
   summaryRprof()
 
 
 data(GermanBonds)
 
-## Diebold/Li estimation
-sv_res <- estim_nss(datadyncouponbonds[[1]], c("GERMANY"), method = "asv",tauconstr = c(0.2,10,0.2,0))
+sv_res <- estim_nss(datadyncouponbonds[[1]], c("GERMANY"), method = "sv",tauconstr = c(1,10,0.2,0.5))
+
+seq(tauconstr[1] + tauconstr[3], tauconstr[2] - tauconstr[3], tauconstr[3])
