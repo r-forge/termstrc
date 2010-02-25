edfca370305253dce945236fc1cc07b5b4fb00ff
joe (Josef Hayden) <josef.hayden@gmail.com>
2010-02-25 09:00:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@539 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/R/gradfunc.R
+++ pkg/R/gradfunc.R
@@ -26,18 +26,18 @@ grad_sv_bonds_grid <- function(beta, tau, m, cf, w, p){
 }
 
 
-grad_sv_bonds <- function(beta,tau,m,cf,w,p){
+grad_sv_bonds <- function(beta,m,cf,w,p){
 
-  emt1 <- exp(-m/tau[1])
-  emt2 <- exp(-m/tau[2])
+  emt1 <- exp(-m/beta[4])
+  emt2 <- exp(-m/beta[6])
   oemt1 <- (1-emt1)
-  t1emt1 <- tau[1]*oemt1
+  t1emt1 <- beta[4]*oemt1
   emt1tm <- (-emt1 + t1emt1/m)
-  emt2tm <- (-emt2 + tau[2]*(1 - emt2)/m)
-  emt1t <- emt1/tau[1]
-  emt2t <- emt2/tau[2]
+  emt2tm <- (-emt2 + beta[6]*(1 - emt2)/m)
+  emt1t <- emt1/beta[4]
+  emt2t <- emt2/beta[6]
 
-  a <- exp((-beta[1] - beta[3]*emt1tm - beta[4]*emt2tm - (beta[2]*t1emt1)/m)*m/100)
+  a <- exp((-beta[1] - beta[3]*emt1tm - beta[5]*emt2tm - (beta[2]*t1emt1)/m)*m/100)
 
  
   acf <- a*cf
@@ -49,8 +49,8 @@ grad_sv_bonds <- function(beta,tau,m,cf,w,p){
   gbeta2 <- sum(b*(-cSums(d*t1emt1, na.rm=TRUE)))
   gbeta3 <- sum(b*(-cSums(dm*emt1tm, na.rm=TRUE)))
   gbeta5 <- sum(b*(-cSums(dm*emt2tm, na.rm=TRUE)))     
-  gbeta4 <- sum(b*(-cSums( dm*(-beta[2]*emt1t + beta[2]*oemt1/m + beta[3]*(-emt1t+ oemt1/m - emt1t*m/tau[1])),na.rm=TRUE)))
-  gbeta6 <- sum(b*(-cSums(dm*beta[4]*( -emt2t + (1-emt2)/m - emt2t*m/tau[2]),na.rm=TRUE)))
+  gbeta4 <- sum(b*(-cSums( dm*(-beta[2]*emt1t + beta[2]*oemt1/m + beta[3]*(-emt1t+ oemt1/m - emt1t*m/beta[4])),na.rm=TRUE)))
+  gbeta6 <- sum(b*(-cSums(dm*beta[5]*( -emt2t + (1-emt2)/m - emt2t*m/beta[6]),na.rm=TRUE)))
 
   c(gbeta1,gbeta2,gbeta3,gbeta4,gbeta5,gbeta6)
   
@@ -66,11 +66,11 @@ cSums <- function (x, na.rm = FALSE, dims = 1L) {
 }
 
 grad_ns_bonds_grid <- function(beta, tau, m, cf, w, p){
-   emt1 <- exp(-m/tau[1])
+   emt1 <- exp(-m/tau)
    oemt1 <- (1-emt1)
-   t1emt1 <- tau[1]*oemt1
+   t1emt1 <- tau*oemt1
    emt1tm <- (-emt1 + t1emt1/m)
-   emt1t <- emt1/tau[1]
+   emt1t <- emt1/tau
  
    a <- exp(-((beta[1] + beta[3]*(-emt1 +t1emt1/m) + 
          (beta[2]*t1emt1)/m)*m)/100)
@@ -88,12 +88,12 @@ grad_ns_bonds_grid <- function(beta, tau, m, cf, w, p){
 }
 
 
-grad_ns_bonds <- function(beta, tau, m, cf, w, p){
-   emt1 <- exp(-m/tau[1])
+grad_ns_bonds <- function(beta, m, cf, w, p){
+   emt1 <- exp(-m/beta[4])
    oemt1 <- (1-emt1)
-   t1emt1 <- tau[1]*oemt1
+   t1emt1 <- beta[4]*oemt1
    emt1tm <- (-emt1 + t1emt1/m)
-   emt1t <- emt1/tau[1]
+   emt1t <- emt1/beta[4]
  
    a <- exp(-((beta[1] + beta[3]*(-emt1 +t1emt1/m) + 
          (beta[2]*t1emt1)/m)*m)/100)
@@ -106,7 +106,7 @@ grad_ns_bonds <- function(beta, tau, m, cf, w, p){
    gbeta1 <- sum(b*(-cSums(dm,na.rm=TRUE)))
    gbeta2 <- sum(b*(-cSums(d*t1emt1, na.rm=TRUE)))
    gbeta3 <- sum(b*(-cSums(dm*emt1tm, na.rm=TRUE)))    
-   gbeta4 <- sum(b*(-cSums( dm*(-beta[2]*emt1t + beta[2]*oemt1/m + beta[3]*(-emt1t+ oemt1/m - emt1t*m/tau[1])),na.rm=TRUE)))
+   gbeta4 <- sum(b*(-cSums( dm*(-beta[2]*emt1t + beta[2]*oemt1/m + beta[3]*(-emt1t+ oemt1/m - emt1t*m/beta[4])),na.rm=TRUE)))
    
    c(gbeta1,gbeta2,gbeta3,gbeta4)
 }
@@ -141,17 +141,17 @@ grad_asv_bonds_grid <- function(beta, tau, m, cf, w, p){
   
 }
 
-grad_asv_bonds <-  function(beta, tau, m, cf, w, p){
-  emt1 <- exp(-m/tau[1])
-  emt2 <- exp(-m/tau[2])
+grad_asv_bonds <-  function(beta, m, cf, w, p){
+  emt1 <- exp(-m/beta[4])
+  emt2 <- exp(-m/beta[6])
   oemt1 <- (1-emt1)
-  t1emt1 <- tau[1]*oemt1
+  t1emt1 <- beta[4]*oemt1
   emt1tm <- (-emt1 + t1emt1/m)
-  emt2tm <- (-exp(-2*m/tau[2]) + (tau[2]*(1 -emt2))/m)
-  emt1t <- emt1/tau[1]
+  emt2tm <- (-exp(-2*m/beta[6]) + (beta[6]*(1 -emt2))/m)
+  emt1t <- emt1/beta[4]
  
   a <- exp(-(beta[1] + beta[3]*emt1tm + 
-         beta[4]*emt2tm + 
+         beta[5]*emt2tm + 
          (beta[2]*t1emt1)/m)*m/100)
 
  
@@ -163,12 +163,34 @@ grad_asv_bonds <-  function(beta, tau, m, cf, w, p){
   gbeta1 <- sum(b*(-cSums(dm,na.rm=TRUE)))
   gbeta2 <- sum(b*(-cSums(d*t1emt1, na.rm=TRUE)))
   gbeta3 <- sum(b*(-cSums(dm*emt1tm, na.rm=TRUE)))
-  gbeta4 <- sum(b*(-cSums( dm*(-beta[2]*emt1t + beta[2]*oemt1/m + beta[3]*(-emt1t+ oemt1/m - emt1t*m/tau[1])),na.rm=TRUE)))
+  gbeta4 <- sum(b*(-cSums( dm*(-beta[2]*emt1t + beta[2]*oemt1/m + beta[3]*(-emt1t+ oemt1/m - emt1t*m/beta[4])),na.rm=TRUE)))
   gbeta5 <- sum(b*(-cSums(dm*emt2tm, na.rm=TRUE)))     
-  gbeta6 <- sum(b*(-cSums(dm*beta[4]*(-emt2/tau[2] + (1-emt2)/m - 2*exp(-2*m/tau[2])*m/tau[2]^2 ),na.rm=TRUE)))
+  gbeta6 <- sum(b*(-cSums(dm*beta[5]*(-emt2/beta[6] + (1-emt2)/m - 2*exp(-2*m/beta[6])*m/beta[6]^2 ),na.rm=TRUE)))
 
   c(gbeta1, gbeta2, gbeta3, gbeta4, gbeta5, gbeta6)
 
 }
 
 
+grad_dl_bonds <- function(beta, lambda, m, cf, w, p){
+   tau <- 1/lambda 
+   emt1 <- exp(-m/tau)
+   oemt1 <- (1-emt1)
+   t1emt1 <- tau*oemt1
+   emt1tm <- (-emt1 + t1emt1/m)
+   emt1t <- emt1/tau
+ 
+   a <- exp(-((beta[1] + beta[3]*(-emt1 +t1emt1/m) + 
+         (beta[2]*t1emt1)/m)*m)/100)
+
+   acf <- a*cf
+   b <- -2*w*(p-cSums(acf,na.rm=TRUE))
+   d <- acf/100
+   dm <- d*m
+
+   gbeta1 <- sum(b*(-cSums(dm,na.rm=TRUE)))
+   gbeta2 <- sum(b*(-cSums(d*t1emt1, na.rm=TRUE)))
+   gbeta3 <- sum(b*(-cSums(dm*emt1tm, na.rm=TRUE)))    
+
+   c(gbeta1,gbeta2,gbeta3)
+}
