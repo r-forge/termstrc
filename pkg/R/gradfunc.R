d58a6788bce2078c88389c342b8a30977605ee31
rbrt (Robert Ferstl) <datarob@gmx.at>
2010-03-12 14:27:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@587 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/R/gradfunc.R
+++ pkg/R/gradfunc.R
@@ -248,27 +248,27 @@ grad_sv <- function(beta, m, y)
 
 grad_sv_grid <- function(beta, tau, m, y)
       {
-      ##   .Call("grad_sv_gridCpp", beta, tau, m, y)
+        .Call("grad_sv_gridCpp", beta, tau, m, y)
         
-        c(sum(-2*(-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - 
-      beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - 
-      (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m + y)),
+##         c(sum(-2*(-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - 
+##       beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - 
+##       (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m + y)),
 
-          sum((-2*tau[1]*(1 - exp(-m/tau[1]))*(-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - 
-        beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - 
-        (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m + y))/m),
+##           sum((-2*tau[1]*(1 - exp(-m/tau[1]))*(-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - 
+##         beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - 
+##         (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m + y))/m),
 
-          sum(-2*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m)*
-    (-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - 
-      beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - 
-      (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m + y)),
+##           sum(-2*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m)*
+##     (-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - 
+##       beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - 
+##       (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m + y)),
 
 
-          sum(-2*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m)*
-    (-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - 
-      beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - 
-      (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m + y))
-          )
+##           sum(-2*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m)*
+##     (-beta[1] - beta[3]*(-exp(-m/tau[1]) + (tau[1]*(1 - exp(-m/tau[1])))/m) - 
+##       beta[4]*(-exp(-m/tau[2]) + (tau[2]*(1 - exp(-m/tau[2])))/m) - 
+##       (beta[2]*tau[1]*(1 - exp(-m/tau[1])))/m + y))
+##           )
       }
 
 
