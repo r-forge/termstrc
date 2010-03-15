d869fb2e10a845a7b84a7c6f69676b541daa2e31
rbrt <rbrt@b6e8c802-fa29-48c1-b741-14f4e927981d>
2010-03-15 16:11:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@593 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc2/code/test_lm.R
+++
@@ -1,8 +0,0 @@
-## to 'lm' or not to 'lm'
-
-y <- rnorm(100)
-X <- cbind(rnorm(100),rnorm(100))
-N <- 1000
-
-system.time(for (i in 1:N) lm(y~X - 1)$coef)           
-system.time(for (i in 1:N) solve(t(X)%*%X)%*%t(X)%*%y)  
