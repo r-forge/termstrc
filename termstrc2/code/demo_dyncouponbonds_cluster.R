ea67ea85b4875adfb1aa57ee1d45d16837083f6f
rbrt (Robert Ferstl) <datarob@gmx.at>
2010-03-15 16:11:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@593 b6e8c802-fa29-48c1-b741-14f4e927981d

--- termstrc2/code/demo_dyncouponbonds_cluster.R
+++
@@ -1,7 +0,0 @@
-rm(list = ls())
-source("termstrcPackage.R")
-load("GermanBonds.RData")
-
-sv_res <- estim_nss(datadyncouponbonds[[1]], c("GERMANY"), method = "sv", deltatau = 0.2, optimtype = "allglobal")
-
-save(sv_res, file = "demo_dyncouponbonds_cluster.RData")
--- termstrc2/code/demo_dyncouponbonds_cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#$ -N dynbonds
-
-R-g --vanilla < demo_dyncouponbonds_cluster.R
-
-#$ -m bae
-#$ -M robert.ferstl@wiwi.uni-regensburg.de
-# -q node.q
