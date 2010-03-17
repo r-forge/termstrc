190f0d87bdf449528c045fa9d0f94fb7fdecb6a0
rbrt <rbrt@b6e8c802-fa29-48c1-b741-14f4e927981d>
2010-03-17 13:05:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@598 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/demo/csplines.R
+++ pkg/demo/csplines.R
@@ -8,4 +8,9 @@ summary(cs_res)
 plot(cs_res)
 
 ## Pricing errors per bond
-plot(cs_res,errors="price",inset=c(.1,.3))
+plot(cs_res,errors="price")
+
+## Remove outliers
+cs_res2 <- estim_cs(rm_bond(govbonds, c("FRANCE"),c("FR0000571044", "FR0000571085")), c("FRANCE"), c(0, 30))
+
+summary(cs_res2)
