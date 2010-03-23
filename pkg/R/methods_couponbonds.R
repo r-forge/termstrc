1a8355e9620a602df9af95f2bdf6e8a796cbbc0b
joe (Josef Hayden) <josef.hayden@gmail.com>
2010-03-23 11:48:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/termstrc@608 b6e8c802-fa29-48c1-b741-14f4e927981d

--- pkg/R/methods_couponbonds.R
+++ pkg/R/methods_couponbonds.R
@@ -1,10 +1,10 @@
 print.dyncouponbonds <- function(x, ...) {
-  cat("This is a dynamic dataset of coupon bonds.\n")
+  cat("This is a dynamic data set of coupon bonds.\n")
   cat(paste("There are",length(x), "observations between",x[[1]][[1]]$TODAY, "and",x[[length(x)]][[1]]$TODAY,".\n"))
 }
 
 print.couponbonds <- function(x, ...) {
-  cat("This is a dataset of coupon bonds for:\n")
+  cat("This is a data set of coupon bonds for:\n")
   cat(names(x),",","\n")
   cat(paste("observed at ", x[[1]]$TODAY,".","\n",sep=""))
  
